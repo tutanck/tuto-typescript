5ac20b061b2527c1aa5d7fb365a12f510f539ecc
tutanck <tutanck@gmail.com>
2023-03-20 16:17:38 +0100

    Afficher dans la compta les nouveaux champs de l'objet fiscal

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,12 @@
-console.log("hello");
+"use strict";
+const compteur = document.querySelector("#compteur");
+let i = 0;
+const increment = (e) => {
+    e.preventDefault();
+    i++;
+    const span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector("span");
+    if (span) {
+        span.innerText = i.toString();
+    }
+};
+compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener("click", increment);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+const compteur = document.querySelector("#compteur");
+
+let i = 0;
+
+const increment = (e: Event) => {
+  e.preventDefault();
+
+  i++;
+
+  const span = compteur?.querySelector("span");
+
+  if (span) {
+    span.innerText = i.toString();
+  }
+};
+
+compteur?.addEventListener("click", increment);
